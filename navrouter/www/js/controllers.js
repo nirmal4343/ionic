a82c045bfcea80ad9c26ed55a797c4150a27d5f7
Nirmal <nirmal.4343@gmail.com>
2014-08-01 16:17:28 -0400

    .

--- navrouter/www/js/controllers.js
+++ navrouter/www/js/controllers.js
@@ -32,5 +32,29 @@ angular.module('MapApp.controllers', [])
         $scope.answer = CalculatorService.cube(num);
     }
 	
+})
+
+
+.controller('ContactController', function ($scope, ContactService) {
+ 
+    $scope.contacts = ContactService.list();
+ 
+    $scope.saveContact = function () {
+        ContactService.save($scope.newcontact);
+        $scope.newcontact = {};
+    }
+ 
+ 
+    $scope.delete = function (id) {
+ 
+        ContactService.delete(id);
+        if ($scope.newcontact.id == id) $scope.newcontact = {};
+    }
+ 
+ 
+    $scope.edit = function (id) {
+        $scope.newcontact = angular.copy(ContactService.get(id));
+    }
 });
+
 
